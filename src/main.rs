e0ab2b7eb5697f8424853f401129f5ce569ea0a8
Friedrich Chen <jaleverchen@gmail.com>
2025-03-07 10:11:05 +0800

    feat: dynamic dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+trait Animal {
+    fn make_sound(&self);
+}
+
+struct Dog;
+
+struct Cat;
+
+impl Animal for Dog {
+    fn make_sound(&self) {
+        println!("Woof!");
+    }
+}
+
+impl Animal for Cat {
+    fn make_sound(&self) {
+        println!("Meow!");
+    }
+}
+
 fn main() {
-    
+    let animals: Vec<Box<dyn Animal>> = vec![Box::new(Dog), Box::new(Cat)];
+    for animal in animals {
+        animal.make_sound();
+    }
 }
