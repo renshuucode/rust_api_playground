e7aa8d375575d674f4fd22c0197ea4abcc8e61a2
Friedrich Chen <jaleverchen@gmail.com>
2025-03-07 10:15:00 +0800

    feat: Printable trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+trait Printable {
+    fn print(&self);
+}
+
+struct Book {
+    title: String,
+}
+
+impl Printable for Book {
+    fn print(&self) {
+        println!("Book title: {}", self.title);
+    }
+}
+
 fn main() {
-    
+    let book = Book {
+        title: String::from("The Great Gatsby"),
+    };
+    book.print();
 }
